6a758b5e38711ec2071cd4fc1c1e88e6f34aa5c5
Suraj Sangale <suraj.sangale@fortune4.in>
2024-04-23 17:07:56 +0530

    two way binding with click log

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -5,7 +5,17 @@
   </div>
   <button v-on:click="onClickButton('clicked')">Save</button>
   <button v-on:dblclick="onClickButton('double click noticed')">Doubled</button>
-  
+  <div> <h1>Log in:</h1>
+  <h3>enail</h3>
+  <input type="email" placeholder="enter your email" v-model="email"/>
+  <h3>password</h3>
+  <input type="password" placeholder="enter your password" v-model="password"/>
+  </div>
+  <div>
+    <button v-on:click="onClickLogIn()">
+      login   
+    </button>
+  </div>
 </template>
 
 
@@ -17,7 +27,17 @@ export default {
     disc: String,
     data: Object,
   },
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
+  },
   methods: {
+    onClickLogIn() {
+      console.log('email', this.email);
+      console.log('password', this.password);
+    },
     onClickButton(name) {
       console.log(name)
     }
